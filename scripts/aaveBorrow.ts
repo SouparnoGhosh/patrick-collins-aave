d211576411942ffaadf34e4b0fd80bd438afff71
SouparnoGhosh <ghosh99souparno@gmail.com>
2022-06-06 13:59:44 +0530

    added pricefeed

--- scripts/aaveBorrow.ts
+++ scripts/aaveBorrow.ts
@@ -19,8 +19,18 @@ async function main() {
   const lendingPool = await getLendingPool(deployer);
   console.log(`Lending Pool address: ${lendingPool.address}`);
 
-  // Now deposit
+  // Now deposit the token
   const wethTokenAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+  // But first, you have to approve the token. Else you will get an error
+  await approveErc20(wethTokenAddress, lendingPool.address, AMOUNT, deployer);
+  console.log(`Depositing...`);
+  await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0);
+  console.log(`Deposited!`);
+  const borrowData = await getBorrowUserData(lendingPool, deployer);
+  const availableBorrowsETH = borrowData[0];
+  const totalDebtETH = borrowData[1];
+  // Get the DAI/ETH price
+  const daiPrice = await getDaiPrice();
 }
 
 async function getLendingPool(account: Address): Promise<ILendingPool> {
@@ -51,9 +61,41 @@ async function approveErc20(
     erc20ContractAddress,
     account
   );
-  const txResponse = erc20Token.approve(spenderAddress, amountToSpend);
+  const txResponse = await erc20Token.approve(spenderAddress, amountToSpend);
   await txResponse.wait(1);
   console.log(`Approved`);
+}
+
+async function getBorrowUserData(
+  lendingPool: ILendingPool,
+  account: Address
+): Promise<[BigNumber, BigNumber]> {
+  const { totalCollateralETH, totalDebtETH, availableBorrowsETH } =
+    await lendingPool.getUserAccountData(account);
+  console.log(
+    `You have ${ethers.utils.formatEther(
+      totalCollateralETH
+    )} worth of ETH deposited.`
+  );
+  console.log(
+    `You have ${ethers.utils.formatEther(totalDebtETH)} worth of ETH borrowed.`
+  );
+  console.log(
+    `You can borrow ${ethers.utils.formatEther(
+      availableBorrowsETH
+    )} worth of ETH.`
+  );
+  return [availableBorrowsETH, totalDebtETH];
+}
+
+async function getDaiPrice(): Promise<BigNumber> {
+  const daiPriceFeed = await ethers.getContractAt(
+    "AggregatorV3Interface",
+    "0x773616e4d11a78f511299002da57a0a94577f1f4"
+  );
+  const price = (await daiPriceFeed.latestRoundData())[1];
+  console.log(`The DAI/ETH price is ${price.toString()}`);
+  return price;
 }
 
 main()
--- scripts/getWeth.ts
+++ scripts/getWeth.ts
@@ -1,7 +1,8 @@
 import { ethers, getNamedAccounts /* network */ } from "hardhat";
 // import { networkConfig } from "../helper-hardhat-config";
+import { BigNumber } from "ethers";
 
-export const AMOUNT = ethers.utils.parseEther("0.02");
+export const AMOUNT = ethers.utils.parseEther("0.02").toString();
 
 export async function getWeth() {
   const { deployer } = await getNamedAccounts();
@@ -15,6 +16,6 @@ export async function getWeth() {
   );
   const tx = await iWeth.deposit({ value: AMOUNT });
   await tx.wait(1);
-  const wethBalance = await iWeth.balanceOf(deployer);
-  console.log(`Got ${wethBalance.toString()} WETH`);
+  const wethBalance: BigNumber = await iWeth.balanceOf(deployer);
+  console.log(`Got ${ethers.utils.formatEther(wethBalance)} WETH`);
 }
